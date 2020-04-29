c8cc977f5959b492fdb5bcc4afda0c7587e62183
Adhiraj <adhirajsingh0105@gmail.com>
2020-04-29 15:19:23 +0530

    Added enemies that will follow the player to attack

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _stoppingDistance;
+    [SerializeField]
+    private float _retreatDistance;
+    public Transform player;
+
+    private float _timeBtwShots;
+    public float _startTimeBtwShots;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        _timeBtwShots = _startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.position) > _stoppingDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, _speed * Time.deltaTime);
+        }
+        else if (Vector3.Distance(transform.position, player.position) < _stoppingDistance && Vector3.Distance(transform.position, player.position) > _retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector3.Distance(transform.position, player.position) < _retreatDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, -_speed * Time.deltaTime);
+        }
+
+        if (_timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            _timeBtwShots = _startTimeBtwShots;
+        }
+        else
+        {
+            _timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy_Bullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    private Transform _player;
+    private Vector3 _target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _target = new Vector3(_player.position.x, _player.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _player.position, _speed * Time.deltaTime);
+
+        if (transform.position.x == _target.x && transform.position.z == _target.z)
+        {
+            DestroyEnemyBullet();
+        }
+    }
+
+    void DestroyEnemyBullet()
+    {
+        Destroy(gameObject);
+        //Also destroy after travelling some distance
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyEnemyBullet();
+        }
+    }
+}
